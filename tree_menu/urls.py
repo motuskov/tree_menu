868158328e7de36b0813d32bfc6bdfdc50a3e16b
Konstantin Motuskov <motuskov@gmail.com>
2023-03-03 20:43:45 -0300

    Create using example.

--- tree_menu/urls.py
+++ tree_menu/urls.py
@@ -17,9 +17,58 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html')),
+    path('pets/<url>', TemplateView.as_view(template_name='index.html')),
+    path('pets/dogs/<url>', TemplateView.as_view(template_name='index.html')),
+    path('pets/cats/<url>', TemplateView.as_view(template_name='index.html')),
+    path(
+        'pets/dogs/shepherd/<url>',
+        TemplateView.as_view(template_name='index.html')
+    ),
+    path(
+        'cities/russia',
+        TemplateView.as_view(template_name='index.html'),
+        name='russia'
+    ),
+    path(
+        'cities/russia/moscow',
+        TemplateView.as_view(template_name='index.html'),
+        name='moscow'
+    ),
+    path(
+        'cities/russia/murmansk',
+        TemplateView.as_view(template_name='index.html'),
+        name='murmansk'
+    ),
+    path(
+        'cities/russia/yaroslavl',
+        TemplateView.as_view(template_name='index.html'),
+        name='yaroslavl'
+    ),
+    path(
+        'cities/australia',
+        TemplateView.as_view(template_name='index.html'),
+        name='australia'
+    ),
+    path(
+        'cities/australia/perth',
+        TemplateView.as_view(template_name='index.html'),
+        name='perth'
+    ),
+    path(
+        'cities/australia/melbourne',
+        TemplateView.as_view(template_name='index.html'),
+        name='melbourne'
+    ),
+    path(
+        'cities/australia/sydney',
+        TemplateView.as_view(template_name='index.html'),
+        name='sydney'
+    ),
 ]
 
 if settings.DEBUG:
